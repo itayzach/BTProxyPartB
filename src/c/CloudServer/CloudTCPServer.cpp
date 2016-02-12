9a4fc6b04a2239377b0f27ed0fa0bfcb33c90663
btproxy <btproxyproject@gmail.com>
2016-02-12 15:50:59 +0200

    updated CloudServer for the complete pipe

--- src/c/CloudServer/CloudTCPServer.cpp
+++ src/c/CloudServer/CloudTCPServer.cpp
@@ -1,12 +1,78 @@
 #define DEFAULT_MSGLEN 512
 #define DEFAULT_IDLEN 7 // btproxy / windspc
 #define DEFAULT_PORT "4020"
+#define COMM_THREADS_NUM 2
 
 #pragma comment(lib, "ws2_32.lib") //Winsock Library
 #include<stdio.h>
 #include<winsock2.h>
-#include <ws2tcpip.h>
+#include<ws2tcpip.h>
+//#include <windows.h>
+
+ DWORD WINAPI CommChannel( LPVOID lpParam );
+
+// Sockets struct for the comm channel threads
+typedef struct Sockets {
+    int SrcSocket;
+    int DstSocket;
+} SocketsStruct, *pSocketsStruct;
  
+ 
+DWORD WINAPI CommChannel( LPVOID lpParam ) {
+	char *msgFromSrc = NULL;
+	int recvmsglen = DEFAULT_MSGLEN;
+	int iResult, iSendResult;
+	// Convert to the correct data type
+	pSocketsStruct pSockets = (pSocketsStruct)lpParam;
+	//printf("[%d->%d] Thread created\n", pSockets->SrcSocket, pSockets->DstSocket);
+	msgFromSrc = (char*)malloc(DEFAULT_MSGLEN);
+	while (true) {
+		// recv message from pc socket
+		
+		
+		if (msgFromSrc == NULL) {
+			printf("[%d->%d] can't allocate msgFromSrc string\n", pSockets->SrcSocket, pSockets->DstSocket);
+			closesocket(pSockets->SrcSocket);
+			closesocket(pSockets->DstSocket);
+			WSACleanup();
+			return 1;
+		}
+		
+		printf("[%d->%d] Waiting for message from src...\n", pSockets->SrcSocket, pSockets->DstSocket);
+		iResult = recv(pSockets->SrcSocket, msgFromSrc, recvmsglen, 0);
+		if (iResult > 0) {
+			//printf("[%d->%d] Bytes received from src: %d\n", pSockets->SrcSocket, pSockets->DstSocket, iResult);
+			msgFromSrc[iResult] = '\0';
+			printf("[%d->%d] Received %s from src\n", pSockets->SrcSocket, pSockets->DstSocket, msgFromSrc);
+		} else {
+			printf("[%d->%d] recv from src failed with error: %d\n", pSockets->SrcSocket, pSockets->DstSocket, WSAGetLastError());
+			closesocket(pSockets->SrcSocket);
+			closesocket(pSockets->DstSocket);
+			WSACleanup();
+			return 1;
+		}
+		// send data from pc socket to btproxy socket
+		printf("[%d->%d] Sending message %s to dst...\n", pSockets->SrcSocket, pSockets->DstSocket, msgFromSrc);
+		iSendResult = send( pSockets->DstSocket, msgFromSrc, strlen(msgFromSrc), 0 );
+		if (iSendResult == SOCKET_ERROR) {
+			printf("[%d->%d] send to dst failed with error: %d\n", pSockets->SrcSocket, pSockets->DstSocket, WSAGetLastError());
+			closesocket(pSockets->SrcSocket);
+			closesocket(pSockets->DstSocket);
+			WSACleanup();
+			return 1;
+		}
+		//printf("[%d->%d] Bytes sent to dst: %d\n", pSockets->SrcSocket, pSockets->DstSocket, iSendResult);
+		if (strcmp(msgFromSrc, "msgFromDLL_closeSockets") == 0 ) {
+			printf("[%d->%d] received msgFromDLL_closeSockets, but not exiting loop for debug\n");
+			//printf("[%d->%d] received msgFromDLL_closeSockets, exiting loop\n");
+			//break;
+		}
+	}
+	printf("[%d->%d] Thread finished\n", pSockets->SrcSocket, pSockets->DstSocket);
+	free(msgFromSrc);
+	return 0;
+}
+ // Main
 int main(int argc , char *argv[])
 {
     WSADATA wsaData;
@@ -22,9 +88,9 @@ int main(int argc , char *argv[])
 
     int iSendResult;
     char recvid[DEFAULT_MSGLEN] = "";
-	char msgfrompc[DEFAULT_MSGLEN] = "";
+	
     int recvidlen = DEFAULT_IDLEN;
-	int recvmsglen = DEFAULT_MSGLEN;
+	
     
 	// Initialize ClientSockets
 	PCClientSocket = INVALID_SOCKET;
@@ -105,20 +171,8 @@ int main(int argc , char *argv[])
             printf("Bytes received: %d\n", iResult);
 			
 			if (strcmp(recvid, "windspc") == 0) {
-				printf("Recieved pc\n");
+				printf("Recieved windspc\n");
 				PCClientSocket = TempClientSocket;
-				printf("Waiting for message from pc...\n");
-				iResult = recv(PCClientSocket, msgfrompc, recvmsglen, 0);
-				if (iResult > 0) {
-					printf("Bytes received from pc: %d\n", iResult);
-					printf("Received from pc: %s\n", msgfrompc);
-				} else {
-					printf("recv from pc failed with error: %d\n", WSAGetLastError());
-					closesocket(PCClientSocket);
-					closesocket(BTProxyClientSocket);
-					WSACleanup();
-					return 1;
-				}
 			} else if (strcmp(recvid,"btproxy") == 0) {
 				BTProxyClientSocket = TempClientSocket;
 				printf("Recieved btproxy\n");
@@ -136,19 +190,67 @@ int main(int argc , char *argv[])
         }
 	}
 	
+	// create two threads for communcation between sockets
+	pSocketsStruct pCommThreadData[COMM_THREADS_NUM];
+    DWORD          dwCommThreadId[COMM_THREADS_NUM];
+    HANDLE         hCommThread[COMM_THREADS_NUM];
     
-	// send data from pc socket to btproxy socket
-	printf("Sending message %s to btproxy...\n", msgfrompc);
-	iSendResult = send( BTProxyClientSocket, msgfrompc, strlen(msgfrompc), 0 );
-	if (iSendResult == SOCKET_ERROR) {
-		printf("send failed with error: %d\n", WSAGetLastError());
-		closesocket(PCClientSocket);
-		closesocket(BTProxyClientSocket);
-		WSACleanup();
-		return 1;
+	for (int i = 0; i < COMM_THREADS_NUM; i++) {
+		pCommThreadData[i] = (pSocketsStruct) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY,
+                sizeof(SocketsStruct));
+		if (pCommThreadData[i] == NULL) {
+			printf("Cannot allocate data for thread %d\n", i);
+			closesocket(PCClientSocket);
+			closesocket(BTProxyClientSocket);
+			WSACleanup();
+			return 1;
+		}
+		if (i == 0) {
+			// DLL->BTP
+			pCommThreadData[i]->SrcSocket = PCClientSocket;
+			pCommThreadData[i]->DstSocket = BTProxyClientSocket;
+		} else {
+			// BTP->DLL
+			pCommThreadData[i]->SrcSocket = BTProxyClientSocket;
+			pCommThreadData[i]->DstSocket = PCClientSocket;
+		}
+		
+		hCommThread[i] = CreateThread( 
+            NULL,                   // default security attributes
+            0,                      // use default stack size  
+            CommChannel,            // thread function name
+            pCommThreadData[i],     // argument to thread function 
+            0,                      // use default creation flags 
+            &dwCommThreadId[i]);    // returns the thread identifier 
+
+
+        // Check the return value for success.
+        // If CreateThread fails, terminate execution. 
+        // This will automatically clean up threads and memory. 
+
+        if (hCommThread[i] == NULL) 
+        {
+            printf("Cannot create thread %d\n", i);
+			closesocket(PCClientSocket);
+			closesocket(BTProxyClientSocket);
+			WSACleanup();
+			return 1;
+        }				
 	}
-	printf("Bytes sent to btproxy: %d\n", iSendResult);
+	WaitForMultipleObjects(COMM_THREADS_NUM, hCommThread, TRUE, INFINITE);
+
     
+	// Close all thread handles and free memory allocations.
+    for(int i=0; i<COMM_THREADS_NUM; i++)
+    {
+        CloseHandle(hCommThread[i]);
+        if(pCommThreadData[i] != NULL)
+        {
+            HeapFree(GetProcessHeap(), 0, pCommThreadData[i]);
+            pCommThreadData[i] = NULL;    // Ensure address is not reused.
+        }
+    }
+	
     // No longer need server socket
     closesocket(ListenSocket);
 
@@ -170,6 +272,5 @@ int main(int argc , char *argv[])
 	closesocket(PCClientSocket);
 	closesocket(BTProxyClientSocket);
     WSACleanup();
-
     return 0;
 }
